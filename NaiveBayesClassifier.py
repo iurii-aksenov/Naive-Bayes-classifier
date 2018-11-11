774957db24c35604d1bfefe47f741a3a1fb47332
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-11 11:55:58 +0300

    Добавил методы по предикту и трайну

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -1,56 +1,62 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
+import math
 
 from Mail import *
 from Data import *
-
-
-class WordsStatistic:
-    hams: Dict[int, int] = dict()
-    spams: Dict[int, int] = dict()
-
-    def add_ham_words(self, words: Dict[int, int]):
-        for word in words:
-            self.add_ham_word(word, words[word])
-
-    def add_spam_words(self, words: Dict[int, int]):
-        for word in words:
-            self.add_spam_word(word, words[word])
-
-    def add_ham_word(self, word: int, count: int = 1):
-        if(word in self.hams):
-            self.hams[word] += count
-        else:
-            self.hams[word] = count
-
-    def add_spam_word(self, word: int, count: int = 1):
-        if(word in self.spams):
-            self.spams[word] += count
-        else:
-            self.spams[word] = count
-
-    def get_unique_words_count(self):
-        ham_keys = self.hams.keys()
-        spam_keys = self.spams.keys()
-        unique_words = list(set(ham_keys).union(spam_keys))
-        return len(unique_words)
-
-    def __str__(self):
-        return "hams: " + str(self.hams) + \
-            "\n" + \
-            "spams: " + str(self.spams)
+from WordsStatistic import *
 
 
 class BayesClassifier:
 
-    words: WordsStatistic = WordsStatistic()
-    subject: WordsStatistic = WordsStatistic()
-    body: WordsStatistic = WordsStatistic()
+    smoothing_ration: float = 1.0
+    all_words = 0.00000000000000001
 
-    def train(self):
-        a = ""
+    word_statistic: WordsStatistic = WordsStatistic()
 
-    def predict(self):
-        b = ""
+    def train(self, train_word_statistic: WordsStatistic):
+        self.word_statistic = train_word_statistic
+        self.all_words = train_word_statistic.get_unique_words_count()
+
+    def get_text_probability(self, words: Dict[int, int]):
+        ham_probability_log: float = 0
+        spam_probability_log: float = 0
+        for word in words:
+            ham_probability_log += math.log(self.___get_ham_probability(word))
+            spam_probability_log += math.log(
+                self.___get_spam_probability(word))
+
+        return (ham_probability_log, spam_probability_log)
+
+    def get_text_probability_with_income_count(self, words: Dict[int, int]):
+        ham_probability_log: float = 0
+        spam_probability_log: float = 0
+        for word in words:
+            ham_probability_log += math.log(
+                self.___get_ham_probability(word) * words[word])
+            spam_probability_log += math.log(
+                self.___get_spam_probability(word) * words[word])
+
+        return (ham_probability_log, spam_probability_log)
+
+    def ___get_ham_probability(self, word: int):
+        word_in_ham_count: int = 0
+        if(word in self.word_statistic.hams):
+            word_in_ham_count = self.word_statistic.hams[word]
+        all_hams = self.word_statistic.get_hams_count()
+
+        return self.__calculate_probability(word_in_ham_count, all_hams)
+
+    def ___get_spam_probability(self, word: int):
+        word_in_spam_count: int = 0
+        if(word in self.word_statistic.spams):
+            word_in_spam_count = self.word_statistic.spams[word]
+        all_spams = self.word_statistic.get_spams_count()
+
+        return self.__calculate_probability(word_in_spam_count, all_spams)
+
+    def __calculate_probability(self, word_in_class: float, words_in_class: int):
+        return (self.smoothing_ration + word_in_class) / \
+            (self.smoothing_ration*self.all_words + words_in_class)
 
 
 def main():
@@ -59,34 +65,64 @@ def main():
 
     folds = list(data.parts.keys())
 
-    fold_1 = folds[0]
+    train__words = WordsStatistic()
+    train_subject_words = WordsStatistic()
+    train_body_words = WordsStatistic()
 
-    fold_statistics_words = WordsStatistic()
-    fold_statistics_subject = WordsStatistic()
-    fold_statistics_body = WordsStatistic()
+    test__words = WordsStatistic()
+    test_subject_words = WordsStatistic()
+    test_body_words = WordsStatistic()
 
-    data_part: DataPart = data.parts[fold_1]
+    test_fold = folds[0]
+    #add_to_word_statistics(data.parts[test_fold], test__words, test_subject_words, test_body_words)
+    for train_fold in folds:
+        if train_fold != test_fold:
+            add_to_word_statistics(
+            data.parts[train_fold], train__words, train_subject_words, train_body_words)
+    classifier = BayesClassifier();
+    classifier.train(train_subject_words);
+
+    test_mails:DataPart = data.parts[test_fold]
+    spam_mails: List[Mail] = test_mails.spams;
+    spam_mail: Dict[int,int] = spam_mails[0];
+
+    probabilities: [float, float] = classifier.get_text_probability(spam_mail)
+
+    print(probabilities)
+
+    # for test_fold in folds:
+    #     add_to_word_statistics(
+    #         data.parts[test_fold], test__words, test_subject_words, test_body_words)
+    #     for train_fold in folds:
+    #         if train_fold != test_fold:
+    #             add_to_word_statistics(
+    #                 data.parts[train_fold], train__words, train_subject_words, train_body_words)
+
+
+def add_to_word_statistics(data_part: DataPart, words_statistic: WordsStatistic, subject_statistic: WordsStatistic, body_statistic: WordsStatistic):
     hams: List[Mail] = data_part.hams
     spams: List[Mail] = data_part.spams
     for item in spams:
-        fold_statistics_body.add_spam_words(item.body_words)
-        fold_statistics_subject.add_spam_words(item.subject_words)
-        fold_statistics_words.add_spam_words(item.words)
+        words_statistic.add_spam_words(item.words)
+        subject_statistic.add_spam_words(item.subject_words)
+        body_statistic.add_spam_words(item.body_words)
 
     for item in hams:
-        fold_statistics_body.add_ham_words(item.body_words)
-        fold_statistics_subject.add_ham_words(item.subject_words)
-        fold_statistics_words.add_ham_words(item.words)
-
+        words_statistic.add_ham_words(item.words)
+        subject_statistic.add_ham_words(item.subject_words)
+        body_statistic.add_ham_words(item.body_words)
 
    # def from_data_part_to_wrods_statistic
-
-    #print(str(fold_statistics_words))
-    print(str(fold_statistics_subject))
-   # print(str(fold_statistics_body))
+    # print(str(fold_statistics_words))
+#print(str(fold_statistics_subject))
+# print(str(fold_statistics_body))
 
 
 main()
 
 
 print("Hello world")
+
+
+# Упростить всё. Сразу читать по фодам и сразу записывать в глобальный диктионари. Можно вывети статистику общую по словам в файл
+
