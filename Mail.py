d53351890f21b48441a4f3705563dc89c0d7cc74
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-11 16:45:04 +0300

    Добавил конструктор

--- Mail.py
+++ Mail.py
@@ -1,16 +1,20 @@
-from typing import List
+from typing import List, Dict
 
 
 class Mail:
-    file_name:str = ""
-    words = dict()
-    subject_words = dict()
-    body_words = dict()
+    file_name: str
+    words: Dict[int, int]
+    subject_words: Dict[int, int]
+    body_words: Dict[int, int]
 
-    def __init__(self, file_name:str, subject_words: List[int] = [], body_words: List[int] = []):
-        self.file_name = file_name;
-        self.add_subject_words(subject_words);
-        self.add_body_words(body_words);
+    def __init__(self, file_name: str, subject_words: List[int] = [], body_words: List[int] = []):
+        self.words = dict()
+        self.subject_words = dict()
+        self.body_words = dict()
+        self.file_name = file_name
+
+        self.add_subject_words(subject_words)
+        self.add_body_words(body_words)
 
     def __add_word(self, word: int):
         if(word in self.words):
